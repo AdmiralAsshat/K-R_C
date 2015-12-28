5910195265a5e21c0cee4c72ac923c6e04f59956
AdmiralAsshat <AsshatAdmiral@gmail.com>
2015-12-28 14:44:07 -0600

    Add comments

--- ex1-19.c
+++ ex1-19.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #define MAXLINE 1000
 
-/* Exercise 1-17. Write a program to print all input lines 
-that are longer than 80 characters */
+/* Exercise 1-19. Write a function reverse(s) that reverses the character
+string s. Use it to write a program that reverses its input a line at a time. */
+
+/*
+TODO: 
+- Rewrite reverse function without length arg passed to follow prompt
+- Figure out why garbage characters show up
+*/
 
 int get_line(char line[], int maxline);
 int reverse(char line[], int len);
@@ -43,6 +49,7 @@ int reverse(char s[], int len)
 	char temp[len];
 
 	for(j = len-2; j >= 0; j--)
+	// (len-2) to strip \n and \0
 	{
 		temp[k++] = s[j];
 	}
