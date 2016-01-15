1e04856eb6745dff212f99a04a792d345ac707bb
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-01-14 23:07:59 -0500

    Fix ex1-19. Still hacky on the indecies.

--- ex1-19.c
+++ ex1-19.c
@@ -4,14 +4,9 @@
 /* Exercise 1-19. Write a function reverse(s) that reverses the character
 string s. Use it to write a program that reverses its input a line at a time. */
 
-/*
-TODO: 
-- Rewrite reverse function without length arg passed to follow prompt
-- Figure out why garbage characters show up
-*/
 
 int get_line(char line[], int maxline);
-int reverse(char line[], int len);
+int reverse(char line[]);
 
 int main(void)
 {
@@ -20,7 +15,7 @@ int main(void)
 
 	while((len = get_line(line, MAXLINE)) > 0)
 	{
-		reverse(line, len);
+		reverse(line);
 	}
 	
 	return 0;
@@ -43,16 +38,20 @@ int get_line(char s[], int lim)
 	return i;
 }
 
-int reverse(char s[], int len)
+int reverse(char s[])
 {
-	int j,k = 0;
-	char temp[len];
+	int j,k, len; 
+	k = 0;
+	char temp[MAXLINE] = "\0"; /* Zero out memory before write */
 
-	for(j = len-2; j >= 0; j--)
-	// (len-2) to strip \n and \0
-	{
-		temp[k++] = s[j];
-	}
+	// First loop, find length of string
+	for(j = 0; s[j] != '\n'; j++)
+		;
+	len = j;
+
+	// Second loop, count backwards from string len
+	for(j = len; j > 0 ; j--)
+		temp[k++] = s[j-1]; /* Arrays start at index 0 */
 
 	printf("%s\n",temp);
 }
