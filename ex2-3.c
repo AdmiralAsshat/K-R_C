52c6109ced0b0e97071b9c1a17690456176aebbc
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-03-19 12:47:59 -0400

    Ex2-3: Dropped arg input. Made test suite instead.

--- ex2-3.c
+++ ex2-3.c
@@ -8,7 +8,6 @@ The allowable digits are 0 through 9, a through f, and A through F.
 */
 
 /*TODO:
-- Handle '0X' and '0x' in string
 - Handle string input
 */
 
@@ -16,31 +15,28 @@ unsigned int htoi(const char s[]);
 
 int main(int argc, char const *argv[])
 {
+/*
 	int output;
-
 	output = htoi(argv[1]);
+*/
 
-	printf("%d\n", output);
+	char *test1 = "99";
+	char *test2 = "AC";
+	char *test3 = "0xAC";
+	char *test4 = "0XFF";
+	char *test5 = "0X99";
+
+	printf("Hex:%s; Decimal:%d\n", test1, htoi(test1));
+	printf("Hex:%s; Decimal:%d\n", test2, htoi(test2));
+	printf("Hex:%s; Decimal:%d\n", test3, htoi(test3));
+	printf("Hex:%s; Decimal:%d\n", test4, htoi(test4));
+	printf("Hex:%s; Decimal:%d\n", test5, htoi(test5));
+
 	return 0;
 }
 
 
 unsigned int htoi(const char s[])
-/*{
-	int i, j = 0;
-
-	for(int i = 0; s[i] != '\0'; i++)
-	{
-		int c = tolower(s[i]);
-
-		if c < 16
-	}
-	int tens = (s / 16) * 10;
-	int ones = (s % 16);
-
-	return (tens + ones);
-}
-*/
 {
 	unsigned int tot = 0;
 	for(int i=0; s[i] != '\0'; i++)
